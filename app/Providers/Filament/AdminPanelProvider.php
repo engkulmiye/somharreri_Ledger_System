974f57298fe9f900029be0e8f5b748484faba6e4
engkulmiye <kulmiyemohamed39@gmail.com>
2026-02-23 16:16:00 +0300

    Add logout redirect URL to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->logoutRedirectUrl('/')
             ->authGuard('web')
             ->darkMode(true)
 
